a88537ebac67e47d14c462408e88ddebbd87b4a5
jurikimurik <makovskyiyurii@gmail.com>
2024-12-10 13:55:18 +0100

    Branch and assotiated file created.

---
+++ Source/OpenAIAPI/Public/WebUIChatCompletions.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "WebUIChatCompletions.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OPENAIAPI_API UWebUIChatCompletions : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+	
+};
