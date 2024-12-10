0befde819c251b8f6bdb286ca6e3b36fa5e24053
jurikimurik <makovskyiyurii@gmail.com>
2024-12-10 14:00:55 +0100

    Code copied from WebUICallCompletions

--- Source/OpenAIAPI/Private/WebUIChatCompletions.cpp
+++ Source/OpenAIAPI/Private/WebUIChatCompletions.cpp
@@ -2,4 +2,121 @@
 
 
 #include "WebUIChatCompletions.h"
+#include "OpenAIUtils.h"
+#include "Http.h"
+#include "Dom/JsonObject.h"
+#include "Serialization/JsonReader.h"
+#include "Serialization/JsonSerializer.h"
+#include "OpenAIParser.h"
+
+
+UWebUIChatCompletions::UWebUIChatCompletions()
+{
+}
+
+UWebUIChatCompletions::~UWebUIChatCompletions()
+{
+}
+
+UWebUIChatCompletions* UWebUIChatCompletions::OpenWebUIChatCompletions(FCompletionWebUiSettings ChatSettingsInput, FString Address)
+{
+	UWebUIChatCompletions* BPNode = NewObject<UWebUIChatCompletions>();
+	BPNode->ChatSettings = ChatSettingsInput;
+	BPNode->Address = Address;
+	return BPNode;
+}
+
+void UWebUIChatCompletions::Activate()
+{
+	// NOTE: ApiKey was deleted because it was not really necessary to have it to connect to Oobabooga's WebUI. 
+
+	// creating the http request
+	auto HttpRequest = FHttpModule::Get().CreateRequest();
+	
+	// set headers
+	FString url = FString::Printf(TEXT("%s/v1/completions"), *Address);
+	HttpRequest->SetURL(url);
+	HttpRequest->SetHeader(TEXT("Content-Type"), TEXT("application/json"));
+
+	//build payload
+	TSharedPtr<FJsonObject> _payloadObject = MakeShareable(new FJsonObject());
+
+		// including the final prompt
+	FString finalPrompt = FString::Printf(TEXT("%s%s%s"), *ChatSettings.startSequence, *ChatSettings.prompt, *ChatSettings.endSequence);
+	_payloadObject->SetStringField(TEXT("prompt"), finalPrompt);
+
+		// including other parameters
+	_payloadObject->SetNumberField(TEXT("max_tokens"), ChatSettings.maxTokens);
+	_payloadObject->SetNumberField(TEXT("temperature"), ChatSettings.temperature);
+	_payloadObject->SetNumberField(TEXT("top_p"), ChatSettings.topP);
+	_payloadObject->SetNumberField(TEXT("seed"), ChatSettings.seed);
+		
+
+	// convert payload to string
+	FString _payload;
+	TSharedRef<TJsonWriter<>> Writer = TJsonWriterFactory<>::Create(&_payload);
+	FJsonSerializer::Serialize(_payloadObject.ToSharedRef(), Writer);
+
+	// commit request
+	HttpRequest->SetVerb(TEXT("POST"));
+	HttpRequest->SetContentAsString(_payload);
+
+	if (HttpRequest->ProcessRequest())
+	{
+		HttpRequest->OnProcessRequestComplete().BindUObject(this, &UWebUIChatCompletions::OnResponse);
+	}
+	else
+	{
+		Finished.Broadcast(false, ("Error sending request"),{});
+	}
+}
+
+void UWebUIChatCompletions::OnResponse(FHttpRequestPtr Request, FHttpResponsePtr Response, bool WasSuccessful)
+{
+	// print response as debug message
+	if (!WasSuccessful)
+	{
+		if (!Response)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Error processing request. No response."));
+			Finished.Broadcast(false,  ("Error processing request. No response."), {});
+			return;
+		}
+		
+		UE_LOG(LogTemp, Warning, TEXT("Error processing request. \n%s \n%s"), *Response->GetContentAsString(), *Response->GetURL());
+		if (Finished.IsBound())
+		{
+			Finished.Broadcast(false, *Response->GetContentAsString(), {});
+		}
+
+		return;
+	}
+
+	TSharedPtr<FJsonObject> responseObject;
+	TSharedRef<TJsonReader<>> reader = TJsonReaderFactory<>::Create(Response->GetContentAsString());
+	if (FJsonSerializer::Deserialize(reader, responseObject))
+	{
+		bool err = responseObject->HasField("error");
+
+		if (err)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("%s"), *Response->GetContentAsString());
+			Finished.Broadcast(false, TEXT("Api error"), {});
+			return;
+		}
+
+		
+		OpenAIParser parser(ChatSettings);
+			//Special method in Parses was created
+		FCompletion _out = parser.ParseWebIUResponse(*responseObject);
+
+		if (_out.text.IsEmpty())
+		{
+			Finished.Broadcast(false, TEXT("Response text is empty."), _out);
+		} else
+		{
+			Finished.Broadcast(true, "", _out);	
+		}
+	}
+}
 
--- Source/OpenAIAPI/Public/WebUIChatCompletions.h
+++ Source/OpenAIAPI/Public/WebUIChatCompletions.h
@@ -4,7 +4,11 @@
 
 #include "CoreMinimal.h"
 #include "Kismet/BlueprintAsyncActionBase.h"
+#include "OpenAIDefinitions.h"
+#include "HttpModule.h"
 #include "WebUIChatCompletions.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FOnWebUiChatResponseRecievedPin, bool, Success, const FString&, errorMessage, const FCompletion, message);
 
 /**
  * 
@@ -13,5 +17,26 @@ UCLASS()
 class OPENAIAPI_API UWebUIChatCompletions : public UBlueprintAsyncActionBase
 {
 	GENERATED_BODY()
+
+public:
+	UWebUIChatCompletions();
+	~UWebUIChatCompletions();
+
+	// Special setting that is being used for simple generation
+	FCompletionWebUiSettings ChatSettings;
+
+	//IP address with port number, where HTTP Request will be sent
+	FString Address = "https://127.0.0.1:5000";
+
+	UPROPERTY(BlueprintAssignable, Category="OpenAI")
+	FOnWebUiChatResponseRecievedPin Finished;
+	
+private:
+	
+	UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true"), Category = "OpenAI")
+	static UWebUIChatCompletions* OpenWebUIChatCompletions(FCompletionWebUiSettings chatSettings, FString Address);
+
+	virtual void Activate() override;
+	void OnResponse(FHttpRequestPtr Request, FHttpResponsePtr Response, bool WasSuccessful);
 	
 };
