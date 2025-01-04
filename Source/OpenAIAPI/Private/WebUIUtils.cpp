821c8b5f05c252604102b0e2b30f4f0e1b37895d
jurikimurik <makovskyiyurii@gmail.com>
2025-01-04 14:15:06 +0100

    Created WebUIUtils

---
+++ Source/OpenAIAPI/Private/WebUIUtils.cpp
@@ -0,0 +1,2 @@
+﻿#include "E:\Projects\AiOogaboogaProject\Intermediate\Build\Win64\x64\AiOogaboogaProjectEditor\Development\UnrealEd\SharedPCH.UnrealEd.Project.ValApi.Cpp20.h"
+#include "WebUIUtils.h"
---
+++ Source/OpenAIAPI/Public/WebUIUtils.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#if PLATFORM_WINDOWS
+#include "Runtime/Core/Public/Windows/WindowsPlatformMisc.h"
+#endif
+
+#if PLATFORM_MAC
+#include "Runtime/Core/Public/Apple/ApplePlatformMisc.h"
+#endif
+
+#if PLATFORM_LINUX
+#include "Runtime/Core/Public/Linux/LinuxPlatformMisc.h"
+#endif
+
+#include "WebUIUtils.generated.h"
+
+UCLASS()
+class OPENAIAPI_API UWebUIUtils : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+public:
+	
+};
