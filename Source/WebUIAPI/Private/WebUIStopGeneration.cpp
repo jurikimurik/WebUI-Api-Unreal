7142d007b782831da6a5f0e48ba03cc0a99a7e4e
jurikimurik <makovskyiyurii@gmail.com>
2025-01-06 21:30:15 +0100

    Stop generation request added.

---
+++ Source/WebUIAPI/Private/WebUIStopGeneration.cpp
@@ -0,0 +1,129 @@
+﻿#include "WebUIStopGeneration.h"
+#include "WebUIParser.h"
+#include "Http.h"
+#include "WebUIUtils.h"
+#include "Dom/JsonObject.h"
+#include "Serialization/JsonReader.h"
+#include "Serialization/JsonSerializer.h"
+
+UWebUIStopGeneration::UWebUIStopGeneration()
+{
+}
+
+UWebUIStopGeneration::~UWebUIStopGeneration()
+{
+}
+
+UWebUIStopGeneration* UWebUIStopGeneration::StopWebUIGeneration(FString Address)
+{
+	UWebUIStopGeneration* BPNode = NewObject<UWebUIStopGeneration>();
+	BPNode->Address = Address;
+	return BPNode;
+}
+
+TSharedPtr<FJsonObject> UWebUIStopGeneration::BuildPayload() const
+{
+	//build payload
+	TSharedPtr<FJsonObject> _payloadObject = MakeShareable(new FJsonObject());
+
+	return _payloadObject;
+}
+
+void UWebUIStopGeneration::CommitRequest(const FString& Verb, const TSharedRef<IHttpRequest, ESPMode::ThreadSafe>& HttpRequest, const FString& _payload)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Payload to send: %s"), *_payload);
+	
+	// commit request
+	HttpRequest->SetVerb(Verb);
+	HttpRequest->SetContentAsString(_payload);
+	
+
+	if (HttpRequest->ProcessRequest())
+	{
+		HttpRequest->OnProcessRequestComplete().BindUObject(this, &UWebUIStopGeneration::OnResponse);
+	}
+	else
+	{
+		Finished.Broadcast(false, ("Error sending request"),{});
+	}
+}
+
+bool UWebUIStopGeneration::CheckResponse(const FHttpResponsePtr& Response, const bool& WasSuccessful) const
+{
+	if (!WasSuccessful)
+	{
+		if (!Response)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Error processing request. No response."));
+			Finished.Broadcast(false,  ("Error processing request. No response."), {});
+		} else {
+			UE_LOG(LogTemp, Warning, TEXT("Error processing request. \n%s \n%s"), *Response->GetContentAsString(), *Response->GetURL());
+			if (Finished.IsBound())
+			{
+				Finished.Broadcast(false, *Response->GetContentAsString(), {});
+			}
+		}
+		return false;
+	}
+	return true;
+}
+
+void UWebUIStopGeneration::Activate()
+{
+	// NOTE: ApiKey was deleted because it was not really necessary to have it to connect to Oobabooga's WebUI. 
+
+	// creating the http request
+	auto HttpRequest = FHttpModule::Get().CreateRequest();
+	
+	// set headers
+	FString url = FString::Printf(TEXT("%s/v1/internal/stop-generation"), *Address);
+	HttpRequest->SetURL(url);
+	HttpRequest->SetHeader(TEXT("Content-Type"), TEXT("application/json"));
+
+	auto _payloadObject = BuildPayload();
+		
+
+	// convert payload to string
+	FString _payload;
+	TSharedRef<TJsonWriter<>> Writer = TJsonWriterFactory<>::Create(&_payload);
+	FJsonSerializer::Serialize(_payloadObject.ToSharedRef(), Writer);
+
+	CommitRequest("POST", HttpRequest,_payload);
+}
+
+void UWebUIStopGeneration::OnResponse(FHttpRequestPtr Request, FHttpResponsePtr Response, bool WasSuccessful) const
+{
+	if (!CheckResponse(Response, WasSuccessful)) return;
+
+	TSharedPtr<FJsonObject> responseObject;
+	TSharedRef<TJsonReader<>> reader = TJsonReaderFactory<>::Create(Response->GetContentAsString());
+	UE_LOG(LogTemp, Warning, TEXT("%s"), *Response->GetContentAsString());
+	
+	if (FJsonSerializer::Deserialize(reader, responseObject))
+	{
+		if (responseObject->HasField(TEXT("error")))
+		{
+			UE_LOG(LogTemp, Warning, TEXT("%s"), *Response->GetContentAsString());
+			Finished.Broadcast(false, TEXT("Api error"), {});
+			return;
+		}
+
+		
+		WebUIParser parser(ChatSettings);
+		//Special method in Parses was created
+		FCompletion _out = parser.ParseWebIUResponse(*responseObject);
+
+		if (_out.Text.IsEmpty())
+		{
+			Finished.Broadcast(false, TEXT("Response text is empty."), _out);
+		} else
+		{
+			Finished.Broadcast(true, "", _out);	
+		}
+	} else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Cannot deserialize object"));
+	}
+}
+
+
---
+++ Source/WebUIAPI/Public/WebUIStopGeneration.h
@@ -0,0 +1,41 @@
+﻿#pragma once
+
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "WebUIDefinitions.h"
+#include "HttpModule.h"
+
+#include "WebUIStopGeneration.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FOnWebUiStopResponseRecievedPin, bool, Success, const FString&, errorMessage, const FCompletion, message);
+
+// Function sends HTTP packet to WebUI on address to stop generation.
+UCLASS()
+class WEBUIAPI_API UWebUIStopGeneration : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+
+public:
+	UWebUIStopGeneration();
+	virtual ~UWebUIStopGeneration() override;
+
+	// Special setting that is being used for simple generation
+	FCompletionGenerationSettings ChatSettings;
+
+	//IP address with port number, where HTTP Request will be sent
+	FString Address = "https://127.0.0.1:5000";
+
+	UPROPERTY(BlueprintAssignable, Category="WebUI")
+	FOnWebUiStopResponseRecievedPin Finished;
+
+private:
+	
+	UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true"), Category = "WebUI")
+	static UWebUIStopGeneration* StopWebUIGeneration(FString Address);
+	
+	TSharedPtr<FJsonObject> BuildPayload() const;
+	void CommitRequest(const ::FString&, const TSharedRef<IHttpRequest, ESPMode::ThreadSafe>&, const FString& _payload);
+	bool CheckResponse(const FHttpResponsePtr& Response, const bool& WasSuccessful) const;
+
+	virtual void Activate() override;
+	void OnResponse(FHttpRequestPtr Request, FHttpResponsePtr Response, bool WasSuccessful) const;
+};
