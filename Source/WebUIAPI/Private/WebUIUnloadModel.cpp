abe672b5f918d6bd85adfef464def1f96c835048
jurikimurik <makovskyiyurii@gmail.com>
2025-01-09 04:40:01 +0100

    Unloading Model Method

---
+++ Source/WebUIAPI/Private/WebUIUnloadModel.cpp
@@ -0,0 +1,115 @@
+﻿#include "WebUIUnloadModel.h"
+
+#include "WebUIParser.h"
+#include "Http.h"
+#include "WebUIUtils.h"
+#include "Dom/JsonObject.h"
+#include "Serialization/JsonReader.h"
+#include "Serialization/JsonSerializer.h"
+
+UIWebUIUnloadModel::UIWebUIUnloadModel()
+{
+}
+
+UIWebUIUnloadModel::~UIWebUIUnloadModel()
+{
+}
+
+UIWebUIUnloadModel* UIWebUIUnloadModel::UnloadWebUIModel(FString Address)
+{
+	UIWebUIUnloadModel* BPNode = NewObject<UIWebUIUnloadModel>();
+	BPNode->Address = Address;
+	return BPNode;
+}
+
+TSharedPtr<FJsonObject> UIWebUIUnloadModel::BuildPayload() const
+{
+	//build payload
+	TSharedPtr<FJsonObject> _payloadObject = MakeShareable(new FJsonObject());
+
+	return _payloadObject;
+}
+
+void UIWebUIUnloadModel::CommitRequest(const FString& Verb, const TSharedRef<IHttpRequest, ESPMode::ThreadSafe>& HttpRequest, const FString& _payload)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Payload to send: %s"), *_payload);
+	
+	// commit request
+	HttpRequest->SetVerb(Verb);
+	HttpRequest->SetContentAsString(_payload);
+	
+
+	if (HttpRequest->ProcessRequest())
+	{
+		HttpRequest->OnProcessRequestComplete().BindUObject(this, &UIWebUIUnloadModel::OnResponse);
+	}
+	else
+	{
+		Finished.Broadcast(false, ("Error sending request"),{});
+	}
+}
+
+bool UIWebUIUnloadModel::CheckResponse(const FHttpResponsePtr& Response, const bool& WasSuccessful) const
+{
+	if (!WasSuccessful)
+	{
+		if (!Response)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Error processing request. No response."));
+			Finished.Broadcast(false,  ("Error processing request. No response."), {});
+		} else {
+			UE_LOG(LogTemp, Warning, TEXT("Error processing request. \n%s \n%s"), *Response->GetContentAsString(), *Response->GetURL());
+			if (Finished.IsBound())
+			{
+				Finished.Broadcast(false, *Response->GetContentAsString(), {});
+			}
+		}
+		return false;
+	}
+	return true;
+}
+
+void UIWebUIUnloadModel::Activate()
+{
+	// NOTE: ApiKey was deleted because it was not really necessary to have it to connect to Oobabooga's WebUI. 
+
+	// creating the http request
+	auto HttpRequest = FHttpModule::Get().CreateRequest();
+	
+	// set headers
+	FString url = FString::Printf(TEXT("%s/v1/internal/model/unload"), *Address);
+	HttpRequest->SetURL(url);
+	HttpRequest->SetHeader(TEXT("Content-Type"), TEXT("application/json"));
+
+	auto _payloadObject = BuildPayload();
+		
+
+	// convert payload to string
+	FString _payload;
+	TSharedRef<TJsonWriter<>> Writer = TJsonWriterFactory<>::Create(&_payload);
+	FJsonSerializer::Serialize(_payloadObject.ToSharedRef(), Writer);
+
+	CommitRequest("POST", HttpRequest,_payload);
+}
+
+void UIWebUIUnloadModel::OnResponse(FHttpRequestPtr Request, FHttpResponsePtr Response, bool WasSuccessful) const
+{
+	if (!CheckResponse(Response, WasSuccessful)) return;
+
+	TSharedPtr<FJsonObject> responseObject;
+	TSharedRef<TJsonReader<>> reader = TJsonReaderFactory<>::Create(Response->GetContentAsString());
+	UE_LOG(LogTemp, Warning, TEXT("%s"), *Response->GetContentAsString());
+	
+	//Special method in Parses was created
+	FString _out = (Response->GetContentAsString());
+
+	if (_out.IsEmpty())
+	{
+		Finished.Broadcast(false, TEXT("Response text is empty."), _out);
+	} else
+	{
+		Finished.Broadcast(true, "", _out);	
+	}
+}
+
+
---
+++ Source/WebUIAPI/Public/WebUIUnloadModel.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "WebUIDefinitions.h"
+#include "HttpModule.h"
+
+#include "WebUIUnloadModel.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FOnWebUiUnloadModelResponseRecievedPin, bool, Success, const FString&, errorMessage, const FString, message);
+
+UCLASS()
+class WEBUIAPI_API UIWebUIUnloadModel : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+
+public:
+	UIWebUIUnloadModel();
+	virtual ~UIWebUIUnloadModel() override;
+	
+	//IP address with port number, where HTTP Request will be sent
+	FString Address = "https://127.0.0.1:5000";
+
+	UPROPERTY(BlueprintAssignable, Category="WebUI")
+	FOnWebUiUnloadModelResponseRecievedPin Finished;
+
+private:
+	
+	UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true"), Category = "WebUI")
+	static UIWebUIUnloadModel* UnloadWebUIModel(FString Address);
+	
+	TSharedPtr<FJsonObject> BuildPayload() const;
+	void CommitRequest(const ::FString&, const TSharedRef<IHttpRequest, ESPMode::ThreadSafe>&, const FString& _payload);
+	bool CheckResponse(const FHttpResponsePtr& Response, const bool& WasSuccessful) const;
+
+	virtual void Activate() override;
+	void OnResponse(FHttpRequestPtr Request, FHttpResponsePtr Response, bool WasSuccessful) const;
+	
+};
